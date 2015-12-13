70dcacc88b3f44ef97ae8427e0bde91548498fb1
HyHydrogen <callum.thepro@hotmail.co.uk>
2015-12-13 00:07:16 +0000

    Added Gui, also changed max zoom

--- LD34/src/me/team/ld34/graphics/GuiRenderer.java
+++ LD34/src/me/team/ld34/graphics/GuiRenderer.java
@@ -1,30 +1,45 @@
-package me.team.ld34.graphics;;
+package me.team.ld34.graphics;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import me.team.ld34.core.Game;
 
 public class GuiRenderer {
 
 	private Game game;
-
+	private BufferedImage topGui;
+	
+	
 	public GuiRenderer(Game game) {
 		this.game = game;
+		try {
+			topGui = ImageIO.read(getClass().getResource("/Real Gui.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void render(Graphics2D g) {
+		g.drawImage(topGui, 0, 0, 800, 41, null);
+		
 		int units = game.getGameManager().getResourceManager().getUnits().getAmount();
 		int power = game.getGameManager().getResourceManager().getEnergy().getAmount();
 
 		int unitsPerSecond = game.getGameManager().getResourceManager().getUnits().getPassiveGain();
-
-		g.setColor(Color.WHITE);
-		g.setFont(new Font("Calibri", Font.BOLD, 16));
 		
-		g.drawString("" + units, 80, 16);
-		g.drawString("" + power, 180, 16);
+		//game.getRenderer().getTileRenderer().tiles[Tile.TILE_PRODUCTION];
+		
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("Calibri", Font.BOLD, 19));
+		
+		g.drawString("" + units, 40, 24);
+		g.drawString("" + power, 182, 24);
 	}
 
 }
